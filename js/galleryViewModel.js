e116b7c91f931334391a92e45731b6b7bbe6b147
Nathan Glick <nxg9997@rit.edu>
2019-03-30 23:39:39 -0400

    added quadtree

--- js/galleryViewModel.js
+++ js/galleryViewModel.js
@@ -20,7 +20,8 @@ function genCards(){
     projects.push(new project("Detached", "2D Platformer", "Programmer", "./img/detached_thumb.jpg", [new link("GitLab", "https://gitlab.com/jdx963/detachedcode")], "Detached is a 2D platformer where the object of the game is to collect body parts to gain the power to pass through different colored blocks."));
     projects.push(new project("Humans vs Zombies", "Simulation", "Developer", "./img/hvz.PNG", [new link("GitHub", "https://github.com/nxg9997/HvZ-igme202")], "This is a project created in Unity to demo basic AI behaviours in a 3D environment. The game includes controls that allows you to control various parts of the game, including how many humans and zombies exist in the environment."));
     projects.push(new project("Audio Visualizer", "Simulation", "Developer", "./img/AVlogo.png", [new link("Website", "./AV/visualizer.html")], "This is a web-based audio visualizer built using JS and the Web Audio API"));
-}
+    projects.push(new project("JS Quadtree", "Simulation", "Developer", "./img/quad_thumb.jpg", [new link("Website", "https://people.rit.edu/~nxg9997/fun/")], "This is a simulation where you can click to destroy sections of the page. It uses a quadtree to determine solids. In addition, basic particle physics are utilized to make small explosions where the mouse is clicked."));
+}//https://people.rit.edu/~nxg9997/fun/
 
 class project{
     constructor(title,genre,role,img,link,description){
